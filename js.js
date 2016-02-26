629da6c874ecda1157675ba2005ed31b9e74b273
adderapple <diablosson1@hotmail.com>
2016-02-26 15:24:34 +1300

    new refinements (27/02/16)

--- js.js
+++ js.js
@@ -1,10 +1,8 @@
-$(document).ready(function typeName(){
-		showText("#myname", " Robbie Keller", 0, 160);
-		typing.play();
-	});
-
-/*$(document).ready(function myMove(){
-	});*/
+$(function onLoad(){
+		showText("#myname", " test test", 0, 160);
+		myMove();
+		audio();
+});
 
 var showText = function(target, message, index, interval) {
 	if (index < message.length) {
@@ -13,40 +11,86 @@ var showText = function(target, message, index, interval) {
 	}
 }
 
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("demo").innerHTML = xhttp.responseText;
-    }
-  };
-  xhttp.open("GET", "ajax_info.txt", true);
-  xhttp.send();
-}
-
-function changeText(){
-	document.getElementById("about").innerHTML = "Tuoba";
-}
-
-/*function clicked(){
-	document.getElementById("about").onclick = ;
-}*/
-
 function myMove(){
 	var elem = document.getElementById("movingBlock");
 	var pos = 255;
-	var id = setInterval(frame, 10);
+	var id = setInterval(frame, 1);
+	var id2 = setInterval(frame2, 1);
 	function frame() {
-		if (pos == 1700) {
+		if (pos == 600) {
 			clearInterval(id);
-		} else {
+		} else if (pos != 600){
 			pos++;
 			elem.style.left = pos +"px";
 		}
 	}
+	function frame2() {
+		if (pos == 1600) {
+			pos--;
+			elem.style.left = pos +"px";
+		} else {
+			frame();
+		}
+	}
 }
 
-var typing = new Audio('keyboard typing slow1.mp3');
-typing.volume = .3;
+function audio() {
+	var typing = new Audio('keyboard typing slow1.mp3');
+	typing.volume = .2;
+	typing.play();
+}
 
-	// $.get("index.html", myCallBack);
+function loadDoc3() {
+	$.getJSON("data.json", function(data) {
+		$.each(data.records, function() {
+			$("#projectstable").append("<tr><td>" +this["Name"] +"</td><td>" +this["City"] +"</td><td>" +this["Country"] +"</td></tr><br>");
+		});
+	});
+}
+
+<!-- NEEDS CORRECTION -->
+/*function loadDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  var url = 'hihihihi.txt';
+
+  xmlhttp.onreadystatechange = fill(xmlhttp.responseText);
+    if (xhttp.readyState == 4 && xhttp.status == 200)
+      fill(xmlhttp.responseText);
+    }
+  }
+
+  xmlhttp.open("GET", "url", true);
+  xmlhttp.send();
+
+function fill(response) {
+			window.alert(response);
+	/*var obj = JSON.parse(response); /*problem here 
+
+	var arr = obj.records;
+		window.alert("fadsfas");
+	var i;
+	var out = "<table><tr><th>Name</th><th>City</th><th>Country</th></tr>";
+
+	for (i = 0; i < arr.length; i++) {
+		out += "<tr><td>" +
+		arr[i].Name +
+		"</td><td>" +
+		arr[i].City +
+		"</td><td>" +
+		arr[i].Country +
+		"</td></tr>";
+	}
+	out += "</table>"
+	document.getElementById("projects").innerHTML = out;
+}
+}*/
+
+<!-- UNUSED -->
+/*function loadDoc2() {
+	$("#").load("data.json");
+}*/
+
+<!-- UNUSED -->
+/*function changeText(){
+	document.getElementById("about").innerHTML = "Tuoba";
+}*/
